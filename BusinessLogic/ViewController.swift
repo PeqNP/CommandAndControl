c02a93782b272a0c8274c23ed157088baa4db719
Eric Chamberlain <eric.chamberlain@hotmail.com>
2018-05-29 16:51:59 -0700

    Final version of how view controller uses 'kitchen'

--- BusinessLogic/ViewController.swift
+++ BusinessLogic/ViewController.swift
@@ -6,8 +6,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var control: PDPCommandAndControl!
+    
+    func inject(control: PDPCommandAndControl) {
+        self.control = control
+        
+        control.delegate = self
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension ViewController: PDPCommandAndControlDelegate {
+    
+    func command(_ action: PDPCommand) {
+        switch action {
+        case .updated(let viewState):
+            // Update the view
+            return
+        case .showMoreInfo:
+            // Show the 'More Information' dialog
+            return
+        case .showImageGallery:
+            // Show the 'Image Gallery'
+            return
+        case .routeToPDPFor(let productID):
+            // Route to the PDP for `productID`
+            return
+        }
     }
 }
 
