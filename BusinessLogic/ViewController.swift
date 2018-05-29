b5c481df844f9fd3e54b07bd97240d4a73c78570
Eric Chamberlain <eric.chamberlain@hotmail.com>
2018-05-29 10:08:48 -0700

    Initial Commit

---
+++ BusinessLogic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusinessLogic
+//
+//  Created by Eric Chamberlain on 5/29/18.
+//  Copyright © 2018 Eric Chamberlain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
